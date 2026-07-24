b988d0161fdeb896c960d0cddcc75d7b5200ba06
BAG Babu <degreebcom2004@gmail.com>
2026-07-24 18:54:34 +0530

    Configure Vercel /tmp storage for Laravel

--- api/index.php
+++ api/index.php
@@ -3,4 +3,17 @@
  * Forward Vercel requests to normal index.php
  */
 error_reporting(E_ALL & ~E_DEPRECATED);
+
+$appStorage = '/tmp/storage';
+if (!is_dir($appStorage)) {
+    mkdir($appStorage, 0777, true);
+    mkdir($appStorage . '/framework/views', 0777, true);
+    mkdir($appStorage . '/framework/cache/data', 0777, true);
+    mkdir($appStorage . '/framework/sessions', 0777, true);
+    mkdir($appStorage . '/logs', 0777, true);
+}
+
+$_ENV['APP_STORAGE'] = $appStorage;
+putenv("APP_STORAGE={$appStorage}");
+
 require __DIR__ . '/../public/index.php';
